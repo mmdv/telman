c7befc37e2a69e94cfab1502f42c0887ab963fef
mmdv <rasulm.pro@gmail.com>
2026-05-19 12:46:47 +0400

    chore: update makefile

--- Makefile
+++ Makefile
@@ -1,17 +1,25 @@
+# Include the .env file. The dash (-include) prevents errors if the file does not exist.
+-include .env
+export CACHE_FILE_PATH
 # Default FILE to CACHE_FILE_PATH if not explicitly provided
 FILE ?= $(CACHE_FILE_PATH)
 
-ifeq ($(strip $(FILE)),)
-$(error Pass filename via FILE=... e.g., make check-free FILE=seen.csv ---OR--- set the CACHE_FILE_PATH environment variable.)
-endif
+build:
+	@go build -o bin/github-username-checker ./cmd/github-username-checker/
 
-check-free:
+clean:
+	rm -rf bin/
+
+_require-file:
+	@[ -n "$(FILE)" ] || { echo "Pass filename via FILE=... e.g., make check-free FILE=seen.csv ---OR--- set the CACHE_FILE_PATH environment variable." >&2; exit 1; }
+
+check-free: _require-file
 	@awk -F, '$$2=="free" {print $$1}' "$(FILE)"
 
 check-available: check-free
 
-check-taken:
+check-taken: _require-file
 	@awk -F, '$$2=="taken" {print $$1}' "$(FILE)"
 
-check-invalid:
+check-invalid: _require-file
 	@awk -F, '$$2=="invalid" {print $$1}' "$(FILE)"
---
+++ cmd/github-username-checker/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"github-username-checker/cmd/internal"
+	"github-username-checker/cmd/internal/cache"
+	"github-username-checker/cmd/internal/config"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	ErrRateLimitExceeded = errors.New("API rate limit exceeded")
+	ErrUnauthorized      = errors.New("invalid or expired token")
+)
+
+type app struct {
+	client *http.Client
+	token  string
+	cache  cache.Manager
+}
+
+const (
+	// Maximum number of simultaneous requests to the GitHub API.
+	// Used to limit the number of concurrent requests on the application level
+	// (goroutines) and the transport level.
+	MaxSimultaneousRequests = 10
+)
+
+// TODO: support multiple files as arguments.
+// TODO: add support for proxy, and for n+ usernames require either a token or a proxy.
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			MaxIdleConns:        100,
+			MaxIdleConnsPerHost: MaxSimultaneousRequests,
+			MaxConnsPerHost:     MaxSimultaneousRequests,
+			IdleConnTimeout:     90 * time.Second,
+			TLSHandshakeTimeout: 10 * time.Second,
+			Proxy:               http.ProxyURL(cfg.Proxy),
+		},
+	}
+
+	cacheManager, err := cache.New("csv", cfg.CacheFilePath)
+	if err != nil {
+		return fmt.Errorf("cache manager: init: %w", err)
+	}
+	defer cacheManager.Close()
+
+	if err = cacheManager.Load(); err != nil {
+		return fmt.Errorf("cache manager: load: %w", err)
+	}
+
+	app := &app{
+		client: client,
+		token:  cfg.Token,
+		cache:  cacheManager,
+	}
+
+	if err = app.processFile(cfg.InputFile); err != nil {
+		return fmt.Errorf("process file: %w", err)
+	}
+
+	return nil
+}
+
+func (a *app) processFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+	defer file.Close()
+
+	count := 0
+	scanner := bufio.NewScanner(file)
+
+	g, ctx := errgroup.WithContext(context.Background())
+	g.SetLimit(MaxSimultaneousRequests)
+
+	// TODO: move the seen map to the app struct, so we dedupe across multiple files.
+	seen := make(map[string]struct{})
+
+	for scanner.Scan() {
+		count++
+		username := strings.TrimSpace(scanner.Text())
+
+		if username == "" {
+			continue
+		}
+
+		if a.cache.Exists(username) {
+			fmt.Printf("%s: skipping, already checked\n", username)
+			continue
+
+		}
+
+		if _, ok := seen[username]; ok {
+			// This username is already checked or the check is in progress.
+			// Ignore it silently to not spam the console.
+			continue
+		}
+		seen[username] = struct{}{}
+
+		if ctx.Err() != nil {
+			break
+		}
+
+		g.Go(func() error {
+			res, err := a.processUsername(ctx, username)
+			// We only return an error from processUsername() if we can't process the
+			// rest of the usernames, so we're aborting the entire file.
+			// TODO: with multiple files, stop processing the other files as well.
+			if err != nil {
+				return fmt.Errorf("aborting on username %q: %w", username, err)
+			}
+
+			// Determine the status based on the check result.
+			status := cache.StatusTaken
+			if !res.Taken {
+				status = cache.StatusFree
+			}
+			if !res.Valid {
+				status = cache.StatusInvalid
+			}
+
+			// Write the result to the cache.
+			if err = a.cache.Save(username, status); err != nil {
+				return fmt.Errorf("save to cache: %w", err)
+			}
+
+			return nil
+		})
+	}
+
+	if count == 0 {
+		return fmt.Errorf("no usernames found in file")
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("read file: %w", err)
+	}
+
+	if err := g.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *app) processUsername(ctx context.Context, username string) (internal.CheckResult, error) {
+	if !isValidGithubUsername(username) {
+		return internal.CheckResult{}, nil
+	}
+
+	taken, err := a.checkTaken(ctx, username)
+	if err != nil {
+		if !errors.Is(err, ErrRateLimitExceeded) &&
+			!errors.Is(err, ErrUnauthorized) &&
+			!errors.Is(err, context.Canceled) {
+			fmt.Printf("check availability for username: %q, error: %v\n", username, err)
+		}
+		return internal.CheckResult{}, err
+	}
+
+	return internal.CheckResult{
+		Valid: true,
+		Taken: taken,
+	}, nil
+}
+
+func (a *app) checkTaken(ctx context.Context, username string) (bool, error) {
+	fmt.Printf("%s: checking availability...\n", username)
+
+	url := fmt.Sprintf("https://api.github.com/users/%s", username)
+
+	req, err := http.NewRequestWithContext(ctx, "HEAD", url, nil)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", a.token))
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return true, nil
+	case http.StatusNotFound:
+		return false, nil
+	case http.StatusForbidden:
+		return false, ErrRateLimitExceeded
+	case http.StatusUnauthorized:
+		return false, fmt.Errorf("%w: %q", ErrUnauthorized, a.token)
+	default:
+		return false, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+}
+
+// isValidGithubUsername checks if a string meets GitHub's username constraints.
+func isValidGithubUsername(username string) bool {
+	length := len(username)
+
+	// GitHub usernames cannot be empty and have a maximum length of 39 characters.
+	if length == 0 || length > 39 {
+		return false
+	}
+
+	// Cannot start or end with a hyphen.
+	if username[0] == '-' || username[length-1] == '-' {
+		return false
+	}
+
+	// Cannot contain consecutive hyphens.
+	if strings.Contains(username, "--") {
+		return false
+	}
+
+	// Check for invalid characters.
+	for i := range length {
+		c := username[i]
+		isLower := c >= 'a' && c <= 'z'
+		isUpper := c >= 'A' && c <= 'Z' // Included in case of mixed-case input
+		isDigit := c >= '0' && c <= '9'
+		isHyphen := c == '-'
+
+		if !isLower && !isUpper && !isDigit && !isHyphen {
+			return false
+		}
+	}
+
+	return true
+}
